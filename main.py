0acb7e19a308af3bcbea37a26857a325a8b2753f
FLSVED <kamel.labiad.uk1@gmail.com>
2024-12-08 00:05:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -499,8 +499,8 @@ def load_sample_subscriptions(self):
         self.manager.manage_subscriptions(sample_data)
         self.update_listbox()
 
-   def update_listbox(self):
-    self.listbox.delete(0, END)
+ def update_listbox(self):
+        self.listbox.delete(0, END)
     for device in self.manager.channels:
         status = "Actif" if device['active'] else "Inactif"
         color = "red" if not device['active'] else "black"
