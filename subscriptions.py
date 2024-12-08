5c2c44b39aa07659ec7c22d12be5d6890eee59d3
FLSVED <kamel.labiad.uk1@gmail.com>
2024-12-08 19:51:19 +0100

    Update subscriptions.py

--- subscriptions.py
+++ subscriptions.py
@@ -1,6 +1,5 @@
 import re
 import logging
-from datetime import datetime
 import aiohttp
 import asyncio
 
@@ -8,30 +7,30 @@ __version__ = "1.1.0"
 
 DEFAULT_SUBSCRIPTIONS = """
 http://new.ivue.co:25461/c
-    00:1A:79:70:E2:97 April 12, 2025, 4:34 pm
-    00:1A:79:58:11:68 September 29, 2025, 11:00 pm
-    00:1A:79:63:E8:E2 August 20, 2025, 6:37 pm
-    00:1A:79:5C:87:23 January 20, 2025, 5:34 pm
-    00:1A:79:59:C2:C5 February 3, 2025, 8:05 pm
-    00:1A:79:B5:2B:14 February 20, 2025, 10:52 pm
-    00:1A:79:54:88:37 January 28, 2025, 4:57 pm
-    00:1A:79:B8:47:BD March 2, 2025, 7:56 pm
-    00:1A:79:25:BD:13 April 12, 2025, 7:50 pm
-    00:1A:79:CA:62:9A March 5, 2025, 1:53 pm
-    00:1A:79:69:4F:57 February 6, 2025, 4:38 pm
-    00:1A:79:75:33:75 February 23, 2025, 8:19 pm
-    00:1A:79:B8:4D:1E August 17, 2025, 4:54 pm
-    00:1A:79:B8:47:6F February 17, 2025, 5:50 pm
-    00:1A:79:C6:E1:C2 April 28, 2025, 8:53 pm
-    00:1A:79:BB:DD:10 May 3, 2025, 9:54 pm
-    00:1A:79:6A:65:52 April 17, 2025, 1:47 pm
-    00:1A:79:5A:57:43 January 2, 2025, 8:21 pm
-    00:1A:79:7E:DB:16 April 15, 2025, 7:53 pm
-    00:1A:79:2E:8A:C2 April 12, 2025, 2:03 pm
-    00:1A:79:9A:19:78 April 2, 2025, 12:56 pm
-    00:1A:79:5D:E0:FB February 22, 2025, 8:32 pm
-    00:1A:79:42:78:51 March 28, 2025, 6:18 pm
-    00:1A:79:08:80:6F February 17, 2025, 12:05 pm
+    00:1A:79:70:E2:97
+    00:1A:79:58:11:68
+    00:1A:79:63:E8:E2
+    00:1A:79:5C:87:23
+    00:1A:79:59:C2:C5
+    00:1A:79:B5:2B:14
+    00:1A:79:54:88:37
+    00:1A:79:B8:47:BD
+    00:1A:79:25:BD:13
+    00:1A:79:CA:62:9A
+    00:1A:79:69:4F:57
+    00:1A:79:75:33:75
+    00:1A:79:B8:4D:1E
+    00:1A:79:B8:47:6F
+    00:1A:79:C6:E1:C2
+    00:1A:79:BB:DD:10
+    00:1A:79:6A:65:52
+    00:1A:79:5A:57:43
+    00:1A:79:7E:DB:16
+    00:1A:79:2E:8A:C2
+    00:1A:79:9A:19:78
+    00:1A:79:5D:E0:FB
+    00:1A:79:42:78:51
+    00:1A:79:08:80:6F
 """
 
 class SubscriptionManager:
@@ -47,26 +46,15 @@ class SubscriptionManager:
 
     def parse_data(self, data):
         url_pattern = r'(http[^\s]+)'
-        mac_pattern = r'([0-9A-Fa-f:]+)\s+([\w\s]+, \d{4}, \d{1,2}:\d{2} [ap]m)'
+        mac_pattern = r'([0-9A-Fa-f:]{17})'
 
         urls = re.findall(url_pattern, data)
         devices = []
 
         mac_matches = re.findall(mac_pattern, data)
-        for mac, expiration_date_str in mac_matches:
-            expiration_date = None
-            if expiration_date_str:
-                try:
-                    # Correct format for the given date strings
-                    expiration_date = datetime.strptime(expiration_date_str.strip(), '%B %d, %Y, %I:%M %p')
-                except ValueError:
-                    logging.warning(f"Date d'expiration non valide pour MAC {mac}: {expiration_date_str}")
-                    expiration_date = None
-
+        for mac in mac_matches:
             devices.append({
                 'mac': mac,
-                'expiration_date': expiration_date,
-                'days_left': (expiration_date - datetime.now()).days if expiration_date else None,
                 'active': True
             })
 
