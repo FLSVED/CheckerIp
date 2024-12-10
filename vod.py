e2d295ca4d041a04bb6e40802198630d35bae6c1
FLSVED <kamel.labiad.uk1@gmail.com>
2024-12-10 19:19:55 +0100

    Update vod.py

--- vod.py
+++ vod.py
@@ -1,11 +1,18 @@
-__version__ = "1.0.0"
+__version__ = "2.0.0"
+
 
 class VodManager:
-    def __init__(self):
-        self.vod_list = []
 
-    def load_vod(self, vod_data):
-        self.vod_list = vod_data.splitlines()
+def __init__(self):
 
-    def search_vod(self, query):
-        return [vod for vod in self.vod_list if query.lower() in vod.lower()]
+self.vod_list = []
+
+
+def load_vod(self, vod_data):
+
+self.vod_list = vod_data.splitlines()
+
+
+def search_vod(self, query):
+
+return [vod for vod in self.vod_list if query.lower() in vod.lower()]
