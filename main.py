76fc74c412e84d3823122da2b0cf095796b79aef
FLSVED <kamel.labiad.uk1@gmail.com>
2024-12-10 19:27:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
-python
 from tkinter import Tk
 from ui import IPTVApp
 from config_manager import ConfigManager
 import logging
+
 def setup_logging():
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 def main():
-setup_logging()
-config_manager = ConfigManager()
-server_url = config_manager.get('server_url')
-mac_address = config_manager.get('mac_address')
+    setup_logging()
+    config_manager = ConfigManager()
+    server_url = config_manager.get('server_url')
+    mac_address = config_manager.get('mac_address')
 
-if not server_url or not mac_address:
-
-logging.error("Server URL or MAC address is missing in the configuration.")
-
-return
-
-
-root = Tk()
-
-app = IPTVApp(root, config_manager)
-
-root.mainloop()
-
+    if not server_url or not mac_address:
+        logging.error("Server URL or MAC address is missing in the configuration.")
+        return
+    
+    root = Tk()
+    app = IPTVApp(root, config_manager)
+    root.mainloop()
 
 if __name__ == "__main__":
-
-main()
+    main()
